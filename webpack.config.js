66f43b8e2da13d615dd3115e29833cc37b3f686a
mirkoterzic <mirko.terzic2002@gmail.com>
2024-04-26 12:21:21 +0200

    added CleanWebpackPlugin and set template to HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "webpack Plugin",
+      template: "./src/index.html",
     }),
   ],
   output: {
