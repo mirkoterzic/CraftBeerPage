06e12e5b7c77db91a28acc844ac6f22152d37eab
mirkoterzic <mirko.terzic2002@gmail.com>
2024-05-02 16:21:06 +0200

    added eventlisteners to all buttons and gave them their function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("from index.js");
+import Home from "./home.js";
+import About from "./about.js";
+import Menu from "./menu.js";
+
+const nav = document.querySelector("nav");
+const btns = document.querySelectorAll("button");
+const content = document.querySelector("#content");
+Home();
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    content.textContent = "";
+    if (btn.textContent === "Home") {
+      Home();
+    } else if (btn.textContent === "Menu") {
+      Menu();
+    } else if (btn.textContent === "About") {
+      About();
+    }
+  });
+});
